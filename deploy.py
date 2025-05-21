dc20d28ac2e482be94dea74ab1b6b911b1e81986
huyshogun <138808296+huyshogun@users.noreply.github.com>
2025-05-21 17:05:24 +0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -42,7 +42,7 @@ def get_response_from_chatbot_gt(user_question):
    response_1 = models.generate_content("Cho câu hỏi: " + user_question + " Bạn hãy cho tôi biết và liệt kê số hiệu và tên của của các khoản và điểm nào trong Nghị định 168/2024/NĐ-CP mà tôi cung cấp dưới đây liên quan hoặc giống lỗi mà người trong câu hỏi mắc phải mà tôi cung cấp dưới đây được không: \n" + case_1)
    # pattern = r"\b6\.[1-9]\d*(?:\.[a-zA-ZđĐ0-9]+)?\b"
    # pattern = r"Điểm\s+([0-9]+(?:\.(?:[0-9]+|[a-zđ]+))*)\s*"
-   pattern = r"\b(?:[1-9]|[1-9][0-9])\.(?:[1-9]|[1-9][0-9])(?:\.[a-zđ]+)?\b"
+   pattern = r"\b[1-9]\d*\.[1-9]\d*(?:\.[a-zđ]+)?\b"
    matches_1 = re.findall(pattern, response_1.text)
    matches1 = []
    for i in matches_1:
@@ -64,7 +64,7 @@ def get_response_from_chatbot_gt(user_question):
                 k = k + 1
         if k > 0:
             ucv = models.generate_content("Cho câu hỏi: " + user_question + "Trong các điểm sau thuộc nghị định 168/2024/NĐ-CP chọn và nêu số hiệu của 1 điểm phù hợp và chi tiết nhất với câu hỏi. \n" + uc)
-            p = r"\b(?:[1-9]|[1-9][0-9])\.(?:[1-9]|[1-9][0-9])(?:\.[a-zđ]+)?\b"
+            p = r"\b[1-9]\d*\.[1-9]\d*(?:\.[a-zđ]+)?\b"
             m = re.findall(p, ucv.text)
             if len(m) > 0:
                 matches1.append(m[0])
