1355387eab0444641ee5a46614d5e6b5ef3065b1
huyshogun <138808296+huyshogun@users.noreply.github.com>
2025-05-21 13:55:45 +0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -3,35 +3,118 @@ import pickle
 import google.generativeai as genai
 import os
 import pdfplumber
-from function import relevant_context, relevant_passage, relevant_passage1, make_first_prompt, relevant_context0, make_first_prompt_gt, make_first_prompt_gt1
+from function import make_first_prompt_gt1
 api_key = "AIzaSyB8AbaE7GCBAmsnBWCl5GwH4cCrnzEdsZY"
 model="models/text-embedding-004"
 models = genai.GenerativeModel('gemini-2.0-flash')
 genai.configure(api_key=api_key)
 with open('knowledge_graph_13_5_25.pkl', 'rb') as f:
     G = pickle.load(f)
-m = 1
-Dieu = ""
-def read_pdf(file_path):
-    text = ""
-    with pdfplumber.open(file_path) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text() + "\n"
-    return text
-
-file_path = "Nghi_dinh_168.pdf"
-content = read_pdf(file_path)
-def get_response_from_chatbot(user_question):
-   response = models.generate_content("Cho câu hỏi: " + user_question + " Những điều nào trong danh sách sau phù hợp để trả lời câu hỏi trên" + Dieu)
-   input = relevant_context(response.text)
-#   input = relevant_context0(response.text)
-   relevant_passage_text = relevant_passage(input)
-   relevant_passage1_text = relevant_passage1(user_question)
-   prompt = make_first_prompt(user_question,relevant_passage_text+"\n"+relevant_passage1_text)
-   answer = models.generate_content(prompt)
-   return answer.text
+case = ""
+neighbors_0 = list(G.neighbors("Chương II"))
+neighbors_1 = []
+for i in neighbors_0:
+   nei = list(G.neighbors(i))
+   neighbors_1 = neighbors_1 + nei
+neighbors_2 = []
+for i in neighbors_1:
+   nei = list(G.neighbors(i))
+   neighbors_2 = neighbors_2 + nei
+case_0 = ""
+for i in neighbors_0:
+   for j in list(G.neighbors(i)):
+       case_0 = case_0 + "Điều " + j + ": " + G.nodes[j]['content'] + "\n"
 def get_response_from_chatbot_gt(user_question):
-   prompt = make_first_prompt_gt1(user_question,content)
+   response = models.generate_content("Cho câu hỏi: " + user_question + " Bạn hãy cho tôi biết và liệt kê số hiệu và tên của của các điều nào trong Nghị định 168/2024/NĐ-CP liên quan đến phương tiện của người vi phạm trong câu hỏi mà tôi cung cấp dưới đây (nếu trong câu hỏi không cho biết phương tiện, hãy ghi số 0), lưu ý nếu câu hỏi chỉ ghi xe máy có nghĩa là xe máy chuyên dùng, chứ không phải xe mô tô: \n" + case_0)
+   case_1 = ""
+   if "**0**" in response.text:
+    print(1)
+   else:
+   # pattern = r"\*\*Điều\s+(\d+):\*\*"
+     pattern = r"Điều\s+(\d+)"
+     matches = re.findall(pattern, response.text)
+     for i in matches:
+        nei = G.neighbors(i)
+        for j in nei:
+           case_1 = case_1 + "Điểm " + j + ": " + G.nodes[j]['content'] + "\n"
+   response_1 = models.generate_content("Cho câu hỏi: " + user_question + " Bạn hãy cho tôi biết và liệt kê số hiệu và tên của của các điều khoản điểm nào trong Nghị định 168/2024/NĐ-CP mà tôi cung cấp dưới đây liên quan hoặc giống lỗi mà người trong câu hỏi mắc phải mà tôi cung cấp dưới đây được không: \n" + case_1)
+   # pattern = r"\b6\.[1-9]\d*(?:\.[a-zA-ZđĐ0-9]+)?\b"
+   pattern = r"Điểm\s+([0-9]+(?:\.(?:[0-9]+|[a-zđ]+))*)\s*"
+   matches_1 = re.findall(pattern, response_1.text)
+   matches1 = []
+   for i in matches_1:
+    neig = list(G.neighbors(i))
+    if(len(neig) > 0):
+        uc = ""
+        k = 0
+        uc = uc + "Điểm " + i + ": " + G.nodes[i]['content'] + ".\n"
+        for j in neig:
+            # Check if the edge has the 'content' attribute before accessing it
+            edge_data = G.get_edge_data(i, j)
+            if edge_data:
+               content = edge_data.get("content", "")
+               if content in (
+               "trừ các hành vi vi phạm quy định tại",
+               "trừ hành vi vi phạm quy định tại"
+                ):
+                uc = uc + "Điểm " + j + ": " + G.nodes[j]['content'] + ".\n"
+                k = k + 1
+        if k > 0:
+            ucv = models.generate_content("Cho câu hỏi: " + user_question + "Trong các điểm sau thuộc nghị định 168/2024/NĐ-CP chọn và nêu số hiệu của 1 điểm phù hợp và chi tiết nhất với câu hỏi. \n" + uc)
+            p = r"Điểm\s+([0-9]+(?:\.(?:[0-9]+|[a-z]+))*)\s*"
+            m = re.findall(p, ucv.text)
+            if len(m) > 0:
+                matches1.append(m[0])
+        else:
+            matches1.append(i)
+    else:
+        matches1.append(i)
+   matches1 = list(set(matches1))
+   relevant_passage = ""
+   o = 1
+   for i in  matches1:
+      relevant_passage = relevant_passage + str(o) + ", "
+      o = o + 1
+      relevant_passage = relevant_passage + "Điểm" + i + ": " + G.nodes[i]["content"] + "."
+      nei = list(G.neighbors(i))
+      if len(nei) > 0:
+         relevant_passage = relevant_passage + " Khi vi phạm điểm này, người vi phạm phải chịu thêm các hình phạt bổ sung sau:\n"
+         for j in nei:
+        # Check if the edge exists before accessing its data
+            if G.has_edge(i, j):
+               edge_data = G.get_edge_data(i, j)
+               relation = edge_data.get("content", "")
+               if relation not in (
+                  "trừ các hành vi vi phạm quy định tại",
+                   "trừ hành vi vi phạm quy định tại"
+               ):
+                  if relation == "Nếu gây tai nạn giao thông," or relation == "Nêu gây tai nạn giao thông,":
+                     relevant_passage = relevant_passage + " " + relation + " " + G.nodes[j]["content"] + "(Điểm " + j + ") "
+                  else:
+                     relevant_passage = relevant_passage + " " + G.nodes[j]["content"] + "(Điểm " + j + "). "
+
+            nei0 = list(G.neighbors(j))
+            if len(nei0) > 0:
+              relevant_passage = relevant_passage + "Và "
+              for l in nei0:  # Iterate through neighbors of j (nei0)
+                # Check if the edge exists before accessing its data
+                 if G.has_edge(j, l):
+                    edge_data = G.get_edge_data(j, l)
+                    # Check if edge_data is not None before accessing 'content'
+                    if edge_data is not None:
+                        relation = edge_data.get("content", "")
+                        if relation not in (
+                          "trừ các hành vi vi phạm quy định tại",
+                          "trừ hành vi vi phạm quy định tại"
+
+                         ):
+                           if relation == "Nêu gây tai nạn giao thông," or relation == "Nếu gây tai nạn giao thông,":
+                              relevant_passage = relevant_passage + " " + relation + " " + G.nodes[l]["content"] + "(Điểm " + l + ")\n"
+                           else:
+                              relevant_passage = relevant_passage + " " + G.nodes[l]["content"] + "(Điểm " + l + ")\n"
+            else: relevant_passage = relevant_passage + "\n"
+      relevant_passage = relevant_passage + "\n"
+   prompt = make_first_prompt_gt1(user_question, relevant_passage)
    answer = models.generate_content(prompt)
    return answer.text
 st.set_page_config(page_title="Chatbot Luật Giao Thông", layout="wide")
